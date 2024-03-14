70e03d1369301b3150f90c81763cf6220a78b044
neohaojun <neohaojun@gmail.com>
2024-03-14 22:14:10 +0800

    add namespaces

--- open/software design/raspberry pi/tracker.cpp
+++ open/software design/raspberry pi/tracker.cpp
@@ -3,11 +3,6 @@
 #include <chrono>
 #include <vector>
 
-using namespace lccv;
-using namespace cv;
-using namespace std::chrono;
-using namespace std;
-
 int main()
 {
 
@@ -19,9 +14,8 @@ int main()
     cam.options->video_height = 270;
     cam.options->framerate = 120;
     cam.options->verbose = true;
-    // cv::namedWindow("Video", cv::WINDOW_NORMAL);
 
-    namedWindow("Control", WINDOW_AUTOSIZE); // create a window called "Control"
+    cv::namedWindow("Control", WINDOW_AUTOSIZE);
     int iLowH = 0;
     int iHighH = 15;
 
@@ -52,55 +46,57 @@ int main()
         }
         else
         {
-            Mat imgHSV;
+            cv::Mat imgHSV;
 
-            cvtColor(image, imgHSV, COLOR_BGR2HSV); // Convert the captured frame from BGR to HSV
+            cv::cvtColor(image, imgHSV, COLOR_BGR2HSV); // Convert the captured frame from BGR to HSV
 
-            Mat imgThresholded;
+            cv::Mat imgThresholded;
 
-            inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); // Threshold the image
+            cv::inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); // Threshold the image
 
             // morphological opening (remove small objects from the foreground)
-            erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-            dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+            cv::erode(imgThresholded, imgThresholded, cv::getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+            cv::dilate(imgThresholded, imgThresholded, cv::getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
             // morphological closing (fill small holes in the foreground)
-            dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-            erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+            cv::dilate(imgThresholded, imgThresholded, cv::getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+            cv::erode(imgThresholded, imgThresholded, cv::getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
-            //imshow("Thresholded Image", imgThresholded); // show the thresholded image
-            //imshow("Original", image);                   // show the original image
-            //cv::imshow("Video",image);
-            
+            // imshow("Thresholded Image", imgThresholded); // show the thresholded image
+            // imshow("Original", image);                   // show the original image
+            // cv::imshow("Video",image);
+
             // threshold(imgHSV, imgThresholded, 150, 255, THRESH_BINARY);
             vector<vector<Point>> contours;
             vector<Vec4i> hierarchy;
-            findContours(imgThresholded, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
+            cv::findContours(imgThresholded, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
             // draw contours on the original image
-            Mat image_copy = image.clone();
-            //drawContours(image_copy, contours, -1, Scalar(0, 255, 0), 2);
+            cv::Mat image_copy = image.clone();
+            // drawContours(image_copy, contours, -1, Scalar(0, 255, 0), 2);
             int area = 0;
             int idx;
-            for (int i = 0; i < contours.size(); i++) {
-                if (area < contours[i].size()) {
+            for (int i = 0; i < contours.size(); i++)
+            {
+                if (area < contours[i].size())
+                {
                     idx = i;
                 }
             }
-            Rect rect = boundingRect(contours[idx]);
-            Point p1, p2;
+            cv::Rect rect = cv::boundingRect(contours[idx]);
+            cv::Point p1, p2;
             p1.x = rect.x;
             p1.y = rect.y;
             p2.x = rect.x + rect.width;
             p2.y = rect.y + rect.height;
-            rectangle(image_copy, p1, p2, Scalar(0, 255, 0), 1);
-            imshow("None approximation", image_copy);
+            cv::rectangle(image_copy, p1, p2, cv::Scalar(0, 255, 0), 1);
+            cv::imshow("None approximation", image_copy);
 
             ch = cv::waitKey(1);
         }
         auto tEndSteady = std::chrono::steady_clock::now();
-        nanoseconds diff = tEndSteady - tStartSteady;
+        std::chrono::nanoseconds diff = tEndSteady - tStartSteady;
         std::cout << (1000000000 / diff.count()) << std::endl;
     }
     cam.stopVideo();
-    cv::destroyWindow("Video");
+    cv::destroyAllWindows();
 }
