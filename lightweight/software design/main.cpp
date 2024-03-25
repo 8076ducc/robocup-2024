dce136b81837fc8c7a888ade6992b4b6f8b6f6b9
FluffyPandaX <oliverzhao2006@gmail.com>
2024-03-25 08:28:58 +0800

    upload

--- lightweight/software design/main.cpp
+++ lightweight/software design/main.cpp
@@ -1,10 +1,7 @@
-//the further the ball, the lower the ir reading
-//closer --> higher resistance --> lower voltage
-//analogRead() voltage of ir 
-void loop() {
-  float maxVoltage = 0;
-  int maxIndex = 0;
+float maxVoltage = 0;
+int maxIndex = 0;
 
+void loop() {
   for (int i = 0; i < 24; i = i + 1) {
     float voltage = analogRead(irPins[i]);
     if (voltage > maxVoltage) {
@@ -15,29 +12,18 @@ void loop() {
 }
 
 ballBearing = (360 / 24) * maxIndex;
-ballDist = 1 / maxVoltage //assume proportional relationship, tune constant 
+ballDist = 1 / maxVoltage //tune constant
 
 struct STMTxData {
-int ballBearing; //variables in struct
+int ballBearing; 
 int ballDist;
 }
 
 union STMTxDataUnion {
-    STMTxData data; //converts into data
-    byte bytes[sizeof(STMTxData)]; //converts into bytes
+    STMTxData data; 
+    byte bytes[sizeof(STMTxData)]; 
 
     STMTxDataUnion(): data() {} 
 };
 
-STMTxDataUnion tx_data; //synonymous to int abc, tx_data is just an object
-
-//void onTeensyReceived(const byte *buf, size_t size)
-//{
-  // Read the payload
-  //STMRxDataUnion data_received;
-  // Don't continue if the payload is invalid
-  //if (size != sizeof(data_received))
-    //return;
-  //std::copy(buf, buf + size, std::begin(data_received.bytes));
-//}
-
+STMTxDataUnion tx_data; 
